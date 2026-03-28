2211067783e8f745195718c8b88e61d37e2c19bb
Harshit <harshitingavale45@gmail.com>
2026-03-28 11:37:10 +0530

    Removed private backend logic

--- Backend.py
+++
@@ -1,2 +0,0 @@
-# suppose this is and backend file and I dont wanna disclose this file to the public repo, and if I add and commit it by mistakely,
-# then you can simply remove it from the public repo but keep it on you working directory as well,
