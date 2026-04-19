4fbc0c6ddaf4e659619216611b5380a7833be3ad
Harshit <harshitingavale2332@gmail.com>
2026-04-19 12:58:21 +0530

    update: added the api call logic by using opensource model qwen and added .vscode file in the gitignore

--- fastapi.py
+++ fastapi.py
@@ -1,17 +1,31 @@
 # This will give calls to the fast api of the project.  
 # writing logic for calling the api's from hugging face
 # code just for the reference
-# import requests
+# for using opensource qwen model
 
-API_URL = "https://api-inference.huggingface.co/models/gpt2"
-headers = {
-    "Authorization": "Bearer YOUR_API_TOKEN"
-}
+try:
+    from openai import OpenAI
+except ImportError:
+    import openai
+    OpenAI = openai.OpenAI
 
-payload = {
-    "inputs": "What is artificial intelligence?"
-}
+# Configured by environment variables
+client = OpenAI(api_key="sk-your_dummy_key_here")
 
-# response = requests.post(API_URL, headers=headers, json=payload)
+messages = [
+    {"role": "user", "content": "Type \"I love Qwen3.6\" backwards"},
+]
 
-# print(response.json())
+chat_response = client.chat.completions.create(
+    model="Qwen/Qwen3.6-35B-A3B",
+    messages=messages,
+    max_tokens=81920,
+    temperature=1.0,
+    top_p=0.95,
+    presence_penalty=1.5,
+    extra_body={
+        "top_k": 20,
+    }, 
+)
+print("Chat response:", chat_response)
+
